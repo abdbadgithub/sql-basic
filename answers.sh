c3abedf0c3bc71a51cb7558aa7da50576ed62929
abd <62624790+abdbadgithub@users.noreply.github.com>
2021-03-20 00:38:30 +0200

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -1,2 +1,14 @@
+select * from students
 
+select * FROM students where Age > 30
+
+select * FROM students WHERE Gender like "F" and age =30
+
+SELECT  Points FROM students where name like "Alex"
+
+INSERT INTO students VALUES ('8','abdallah','27','M','1000');
+
+UPDATE  students set Points = 400 where name like "Basma"
+
+UPDATE  students set Points = 150 where name like "Alex"
 
